23c407aeae3803f3288d3ff4b30e9a35d81db4e3
Elian <elianalejandro202@gmail.com>
2023-08-24 09:41:16 -0600

    agregamos muchos archivos

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const { initializeApp } = require('firebase-admin')
+const {initalizeApp, applicationDefault} = require('firebase-admin/app')
+const {getFirestore} = require('firebase-admin/firestore')
+
+initializeApp({
+    credential: applicationDefault()
+})
+
+const db=getFirestore()
+module.exports = {
+    db
+}
